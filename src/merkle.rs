0cb401118c7a13f629074c24df2678f15193fcad
D3cl4n <dmurphyp546@gmail.com>
2026-09-07 17:53:50 -0400

    starting Merkle tree work

--- src/merkle.rs
+++ src/merkle.rs
@@ -1,2 +1,3 @@
-//use plonky2_field::goldilocks_field::GoldilocksField;
-//use plonky2_field::types::Field;
+use plonky2_field::goldilocks_field::GoldilocksField;
+use plonky2_field::types::Field;
+use sha2::{Sha256, Digest};
