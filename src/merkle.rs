7036fd733daf76f3c4d1f026f258d47d3209506f
D3cl4n <dmurphyp546@gmail.com>
2026-09-09 15:36:04 -0400

    working on merkle tree

--- src/merkle.rs
+++ src/merkle.rs
@@ -3,16 +3,17 @@ use plonky2_field::types::Field;
 use sha2::{Sha256, Digest};
 
 
-// structure for the merkle tree nodes
-pub struct MerkleNode {
-    // sha256 hash of the data, left child, right child (can be None)
-    pub hash: Digest,
-    pub left_child: MerkleNode,
-    pub right_child: MerkleNode
+// hash type for the Merkle tree
+pub type Hash = [u8; 32];
+
+
+// compute the hash of a leaf node
+pub fn hash_leaf_node(value: GoldilocksField) -> Hash {
+    let mut hasher = Sha256::new();
+    hasher.update(value.to_canonical_u64().to_le_bytes());
+    hasher.finalize().into(); // TODO: document what .into() does here and how it converts into a Hash type
 }
 
 
-// construct a node given the data to be hashed for storage in the node
-pub fn construct_merkle_node(data: GoldilocksField) -> MerkleNode {
-    let mut node: MerkleNode = 
-}
+// compute the hash of a node with 2 children
+pub fn hash_parent_node(left: )
